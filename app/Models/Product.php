0535f54902a89ca53acb07bf125f30ff74f41712
JohnSuperville <jonathan.superville@gmail.com>
2025-10-09 15:11:33 -0400

    week_2_work

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -32,14 +32,43 @@ class Product extends Model
     /**
      *  =============== SCOPES  ===============
      */
+    public function scopeWithPrices(Builder $query, array $groups_ids = [1])
+    {
+        $query->where('products.id', '>', 0);
+    }
+    public function scopeSingleProduct(Builder $query, int $id)
+    {
+        $query->where('products.id',  $id);
+    }
 
 
     /**
      *  =============== FUNCTIONS  ===============
      */
+    public function getImage()
+    {
+        return asset('storage' . $this->image_path . $this->image_name);
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function getStockPrice()
+    {
+        return $this->price;
+    }
 
 
+    public function getCartQuantityPrice()
+    {
+        return $this->getPrice() * $this->pivot->quantity;
+    }
 
 
-
+    public function getLink()
+    {
+        return route('shop.details', ['id' => $this->id]);
+    }
 }
