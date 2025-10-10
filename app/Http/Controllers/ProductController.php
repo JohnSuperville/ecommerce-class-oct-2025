2b17b9de5488d48605e3c809a38f06029cb14513
JohnSuperville <jonathan.superville@gmail.com>
2025-10-09 20:27:31 -0400

    class 4

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,13 +8,21 @@ use Illuminate\Support\Facades\Auth;
 class ProductController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of all the products or resources.
+     * check if user is login
+     * Get all products from products table.
+     * Pass the infomation to the page called productspage
      */
+
     public function index()
     {
+        //checking to see if person is login, and what group the user belongs to
         $group_ids = Auth::check() ? Auth::user()->getGroups() : [1];
+
+        //this line fetch all products from products table
         $product_data = Product::withPrices()->get();
 
-        return view('pages.testing.productspage', compact('product_data'));
+        //pass product info to view or browser
+        return view('pages.default.productspage', compact('product_data'));
     }
 }
