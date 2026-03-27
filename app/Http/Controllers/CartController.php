283cffeeb00d499d35068efaa9014e8389652429
JohnSuperville <jonathan.superville@gmail.com>
2026-03-26 22:01:12 -0400

    Resolved bug in cart controller

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -7,8 +7,6 @@ use App\Models\Cart;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
